77986643f7fd4434b1106800ec8bab705788c0f7
Ankishoop <ankishkhandelwal705@gmail.com>
2024-06-01 00:36:46 +0530

    fix minors

--- src/models/user.modal.js
+++ src/models/user.modal.js
@@ -55,7 +55,7 @@ const userSchema = new Schema(
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
 
-  this.password = bcrypt.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
@@ -63,8 +63,8 @@ userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
 
-userSchema.method.generateAcessToken = function () {
-  return jwt.sign(
+userSchema.method.generateAcessToken = async function () {
+  return await jwt.sign(
     {
       _id: this._id,
       email: this.email,
