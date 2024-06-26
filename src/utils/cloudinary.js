937c4e4fb7763da91e3f84d791d273c4d5a78329
Ankishoop <ankishkhandelwal705@gmail.com>
2024-06-26 15:28:54 +0530

    add aggregation for watchhistory and channel

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -24,4 +24,28 @@ const uploadOnCloudinary = async (localPath) => {
   }
 };
 
-export { uploadOnCloudinary };
+const deleteFromCloudinary = async (url) => {
+  try {
+    const public_id = extractPublicId(url);
+    const response = await cloudinary.uploader.destroy(public_id);
+    // console.log("🚀 ~ deleteFromCloudinary ~ response:", response);
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const extractPublicId = (url) => {
+  // Match the part of the URL after 'upload/' and before the file extension
+  const regex = /\/upload\/(?:v\d+\/)?([^\.]+)\./;
+  const match = url.match(regex);
+  return match ? match[1] : null;
+};
+
+// const url = "https://res.cloudinary.com/demo/image/upload/v1625647879/sample.jpg";
+// const publicId = extractPublicId(url);
+
+// console.log(publicId); // Output: sample
+
+// xyfj4xt2tnplpkpfkrqy
+export { uploadOnCloudinary, deleteFromCloudinary, extractPublicId };
